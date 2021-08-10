df2334a34ac99a0bf23a4a6fece80ca11bd565b8
lch-howger <360000521@qq.com>
2021-08-10 15:24:14 +0800

    update generate component top-navigation-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavigationBarComponent } from './top-navigation-bar/top-navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavigationBarComponent
   ],
   imports: [
     BrowserModule,
